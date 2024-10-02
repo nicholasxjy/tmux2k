e3a19ee1fc305f202c41a4e24420f21666a4a574
nicholasxjy <nicholasxjy@gmail.com>
2024-10-02 09:56:19 +0800

    tweak some

--- scripts/time.sh
+++ scripts/time.sh
@@ -9,29 +9,29 @@ show_timezone=$(get_tmux_option "@tmux2k-show-timezone" false)
 show_day_month=$(get_tmux_option "@tmux2k-day-month" false)
 
 get_timezone() {
-    if $show_timezone; then
-        date +"%Z "
-    fi
+  if $show_timezone; then
+    date +"%Z "
+  fi
 }
 
 main() {
-    timezone=$(get_timezone)
+  timezone=$(get_timezone)
 
-    if $show_military; then
-        tmux set-option -g clock-mode-style 24
-    else
-        tmux set-option -g clock-mode-style 12
-    fi
+  if $show_military; then
+    tmux set-option -g clock-mode-style 24
+  else
+    tmux set-option -g clock-mode-style 12
+  fi
 
-    if $show_day_month && $show_military; then
-        date +" %a %d/%m %R ${timezone}"
-    elif $show_military; then
-        date +" %a %m/%d %R ${timezone}"
-    elif $show_day_month; then
-        date +" %a %b %d %I:%M %p ${timezone}"
-    else
-        date +" %a %I:%M %p ${timezone}"
-    fi
+  if $show_day_month && $show_military; then
+    date +" %a %d/%m %R ${timezone}"
+  elif $show_military; then
+    date +" %a %m/%d %R ${timezone}"
+  elif $show_day_month; then
+    date +" %a %b %d %I:%M %p ${timezone}"
+  else
+    date +" %a %I:%M %p ${timezone}"
+  fi
 }
 
 main
